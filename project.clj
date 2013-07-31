240b9cbf65b5e8ff1b1edf03710d0fc5627010c5
Mariano Guerra <mariano@marianoguerra.org>
2013-07-31 18:37:29 +0200

    compile against java 1.6

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject org.marianoguerra/clj-rhino "0.2.0"
+(defproject org.marianoguerra/clj-rhino "0.2.1"
   :description "library to ease the interaction between rhino and clojure"
   :url "http://github.com/marianoguerra/clj-rhino"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.mozilla/rhino "1.7R4"]]
   
-  :source-paths      ["src/clj"]
-  :java-source-paths ["src/java"])
+  :javac-options      ["-target" "1.6" "-source" "1.6"]
+  :source-paths       ["src/clj"]
+  :java-source-paths  ["src/java"])
