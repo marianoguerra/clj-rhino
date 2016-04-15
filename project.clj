70c52c825ee630c69c7e3d32ef88121933d548a9
Mariano Guerra <mariano@marianoguerra.org>
2016-04-15 16:29:43 +0200

    update to latest clojure and rhino

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.marianoguerra/clj-rhino "0.2.2"
+(defproject org.marianoguerra/clj-rhino "0.2.3"
   :description "library to ease the interaction between rhino and clojure"
   :url "http://github.com/marianoguerra/clj-rhino"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.mozilla/rhino "1.7R4"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.mozilla/rhino "1.7.7.1"]]
   
   :javac-options      ["-target" "1.6" "-source" "1.6"]
   :source-paths       ["src/clj"]
